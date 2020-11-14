a203693017283aff9e6bc55ee1b7139a0d139460
Pavlushkaaa <pavlikosar@gmail.com>
2020-11-14 13:00:02 +0200

    Version 0.9 | Fixed bugs and added new functionality

--- Assets/Scripts/MoveCamera.cs
+++ Assets/Scripts/MoveCamera.cs
@@ -6,114 +6,140 @@ public class MoveCamera : MonoBehaviour {
 	private float _heightCamera;
 	
 	[SerializeField]
-	private float _speedMove;
+	private float _movementSpeed;
 	
 	[SerializeField]
-	private const float _normilizeMove = 100;
+	private float _movementRaycastSpeed;
+	
+	[SerializeField]
+	private bool IsRaycast = false;
 	
 	[SerializeField]
 	[Range(0,20)]
 	private float _speedLerp;
 	
-	private Vector3 _moveTarget;
-	
-	[SerializeField]
+	private Vector3 _dragStartPosition;
+	private Vector3 _dragCurrentPosition;
+		
 	private Transform _camera;
 	
-	private void LateUpdate()
-	{							
+	private Vector3 _moveTarget;	
+	
+	private void Update()
+	{	
+		MoveMous();
+		MoveMouseSidesScreen();
 		MoveInputKey();
-		MoveMouseEndScreen();
-		transform.position = Vector3.Slerp(transform.position, _moveTarget, _speedLerp * Time.deltaTime);
 		
-		transform.position = new Vector3(transform.position.x, _heightCamera, transform.position.z);
+		MoveSlerp();
+		
+		transform.position = new Vector3(transform.position.x, _heightCamera, transform.position.z); // шоб по У завжди було одне значення
 	}
 	
 	private void Start()
 	{
 		_moveTarget = transform.position;
-	}
+		
+		_camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Transform>();
+	} 
 	
-	private void MoveMouseEndScreen()
+	private void MoveMous()
+	{	
+		if (IsRaycast && !Input.GetKey(KeyCode.LeftShift))
+		{
+			if (Input.GetMouseButtonDown(0)) 
+			{
+				var plane = new Plane(Vector3.up, Vector3.zero);
+				
+				Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+				
+				float entry;
+				
+				if (plane.Raycast(ray, out entry))
+				{
+					_dragStartPosition = ray.GetPoint(entry);
+				}
+			}
+			
+			if (Input.GetMouseButton(0)) 
+			{
+				var plane = new Plane(Vector3.up, Vector3.zero);
+				
+				Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+				
+				float entry;
+				
+				if (plane.Raycast(ray, out entry))
+				{
+					_dragCurrentPosition = ray.GetPoint(entry);
+				}
+				
+				_moveTarget = (transform.position + _dragStartPosition - _dragCurrentPosition) * _movementRaycastSpeed;
+			}
+		}
+	} 
+	
+	private void MoveMouseSidesScreen()
 	{
-		if (Input.GetMouseButton(0)) 
+		if (Input.GetMouseButton(0) && Input.GetKey(KeyCode.LeftShift))
 		{
 			if (Input.mousePosition.y >= Screen.height - 3)
 			{
-				MoveForward(true);
+				MoveForward();
 			}
 			else if (Input.mousePosition.y <= (Screen.height + 3) - Screen.height)
 			{
-				MoveForward(true,-1);
+				MoveForward(-1);
 			}
-			if (Input.mousePosition.x >= Screen.width - 2)
+			else if (Input.mousePosition.x >= Screen.width - 2)
 			{
-				MoveRight(true);
+				MoveRight();
 			}
 			else if (Input.mousePosition.x <= 3)
 			{
-				MoveRight(true,-1);
+				MoveRight(-1);
 			}
 		}
-	}
-		
+	} 
+	
 	private void MoveInputKey()
 	{
 		if (!Input.GetMouseButton(0)) 
 		{
-			if (Input.GetAxis("Vertical") >= 0.1f || Input.GetAxis("Vertical") <= -0.1f)
+			if (Input.GetKey(CameraControlKeys.Up)|| Input.GetKey(KeyCode.UpArrow))
 			{				
-				if (Input.GetAxis("Horizontal") >= 0.1f || Input.GetAxis("Horizontal") <= -0.1f)
-				{
-					_moveTarget = new Vector3( ((_camera.forward.x * Input.GetAxis("Vertical") * _speedMove) * Time.deltaTime) * _normilizeMove + (_camera.right.x * Input.GetAxis("Horizontal") * _speedMove) * Time.deltaTime * _normilizeMove + transform.position.x , transform.position.y,
-				                      	   transform.position.z +  (_camera.right.z * Input.GetAxis("Horizontal") * _speedMove) * Time.deltaTime * _normilizeMove + (_camera.forward.z * Input.GetAxis("Vertical") * _speedMove) * Time.deltaTime * _normilizeMove);
-				}else
-				{
-					MoveForward();
-				}
-				
+				MoveForward();
 			}
-			else if (Input.GetAxis("Horizontal") >= 0.1f || Input.GetAxis("Horizontal") <= -0.1f)
+			
+			if (Input.GetKey(CameraControlKeys.Down)|| Input.GetKey(KeyCode.DownArrow))
+			{				
+				MoveForward(-1);
+			}
+			
+			if (Input.GetKey(CameraControlKeys.Right) || Input.GetKey(KeyCode.RightArrow))
 			{
-				if (Input.GetAxis("Vertical") >= 0.1f || Input.GetAxis("Vertical") <= -0.1f)
-				{
-					_moveTarget = new Vector3( ((_camera.forward.x * Input.GetAxis("Vertical") * _speedMove) * Time.deltaTime) * _normilizeMove + (_camera.right.x * Input.GetAxis("Horizontal") * _speedMove) * Time.deltaTime * _normilizeMove + transform.position.x , transform.position.y,
-				                      	   transform.position.z +  (_camera.right.z * Input.GetAxis("Horizontal") * _speedMove) * Time.deltaTime * _normilizeMove + (_camera.forward.z * Input.GetAxis("Vertical") * _speedMove) * Time.deltaTime * _normilizeMove);
-								
-				}
-				else
-				{
-					MoveRight();
-				}
+				MoveRight();
 			}
+			
+			if (Input.GetKey(CameraControlKeys.Left) || Input.GetKey(KeyCode.LeftArrow))
+			{
+				MoveRight(-1);
+			}		
 		}
 	}
 	
-	private void MoveRight(bool isMous = false, float mousMul = 1)
+	private void MoveRight(float value = 1)
 	{
-		if (!isMous) 
-		{
-			_moveTarget = new Vector3( ((_camera.right.x * Input.GetAxis("Horizontal") * _speedMove) * Time.deltaTime) * _normilizeMove + transform.position.x , transform.position.y,
-				                      	   transform.position.z + (_camera.right.z * Input.GetAxis("Horizontal") * _speedMove) * Time.deltaTime * _normilizeMove);
-		}
-		else
-		{
-			_moveTarget = new Vector3( ((_camera.right.x * mousMul * _speedMove) * Time.deltaTime) * _normilizeMove + transform.position.x , transform.position.y,
-				                      	   transform.position.z + (_camera.right.z * mousMul * _speedMove) * Time.deltaTime * _normilizeMove);
-		}
+		_moveTarget += (_camera.right * value * _movementSpeed);
 	}
 	
-	private void MoveForward(bool isMous = false, float mousMul = 1)
+	private void MoveForward(float value = 1)
 	{
-		if (!isMous) 
-		{
-			_moveTarget = new Vector3( ((_camera.forward.x * Input.GetAxis("Vertical") * _speedMove) * Time.deltaTime) * _normilizeMove + transform.position.x , transform.position.y,
-				                      	   transform.position.z + (_camera.forward.z * Input.GetAxis("Vertical") * _speedMove) * Time.deltaTime * _normilizeMove);
-		}
-		else
-		{
-			_moveTarget = new Vector3( ((_camera.forward.x * mousMul * _speedMove) * Time.deltaTime) * _normilizeMove + transform.position.x , transform.position.y,
-				                      	   transform.position.z + (_camera.forward.z * mousMul * _speedMove) * Time.deltaTime * _normilizeMove);
-		}
+		_moveTarget += (_camera.forward * value * _movementSpeed);
 	}
+		
+	private void MoveSlerp()
+	{
+		transform.position = Vector3.Lerp(transform.position, _moveTarget, _speedLerp * Time.fixedDeltaTime);
+	} 		
 }
--- Assets/Scripts/RotateCamera.cs
+++ Assets/Scripts/RotateCamera.cs
@@ -1,5 +1,4 @@
-﻿
-using UnityEngine;
+﻿using UnityEngine;
 
 public class RotateCamera : MonoBehaviour {
 	
@@ -9,72 +8,127 @@ public class RotateCamera : MonoBehaviour {
 	
 	[SerializeField]
 	[Range(0,100)]
-	private float _speedRotationKeyDown;
+	private float _rotationSpeed;
 	
 	[SerializeField]
 	[Range(0,100)]
-	private float _speedRotationMous;
+	private float _rotationSpeedKey;
 	
 	[SerializeField]
 	[Range(-1,1)]
-	private float _sensitivityMousRotationRigth;
+	private float _sensitivityRotationMous;
 	
 	[SerializeField]
-	[Range(-1,1)]
-	private float _sensitivityMousRotationLeft;
+	private float _zoomSpeed;
 	
-	private float _speedNormalizeRotation = 100;
+	[SerializeField]
+	private float _maxZoom;
+	
+	[SerializeField]
+	private float _minZoom;
+	
+//	private Vector3 _rotationStartPosition;
+//	private Vector3 _rotationCurrentPosition;
+	
+	private Vector3 _zoomTarget;
 		
 	private Quaternion _rotationTarget;
 	
 	private void Update () 
 	{
+		RotationMouse();
 		RotationKeyDown();
-		MousRotate();
+		Zoom();
+		ZoomLepr();
 	}
 	
 	private void RotationKeyDown()
 	{
-		if (!Input.GetMouseButton(1)) 
-		{
-			if (Input.GetKey(KeyCode.Q))
-			{
-				
-				_rotationTarget = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y + (-_speedRotationKeyDown * Time.deltaTime) * _speedNormalizeRotation, transform.eulerAngles.z);
+			if (Input.GetKey(CameraControlKeys.RotationLeft))
+			{	
+				_rotationTarget = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y -_rotationSpeedKey, transform.eulerAngles.z);
 			}
-			else if(Input.GetKey(KeyCode.E))
+			else if(Input.GetKey(CameraControlKeys.RotationRight))
 			{		
-				_rotationTarget = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y + (_speedRotationKeyDown * Time.deltaTime) * _speedNormalizeRotation, transform.eulerAngles.z);
+				_rotationTarget = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y + _rotationSpeedKey, transform.eulerAngles.z);
 			}
-			transform.rotation = Quaternion.Slerp(transform.rotation, _rotationTarget, _speedLerp * Time.deltaTime);	
-		}		
+			
+			RotationSlerp();		
 	}
 	
-	private void MousRotate()
+	private void RotationMouse()
 	{	
-		if (Input.GetMouseButton(1))
-		{		
-			if (Input.mousePosition.x >= Screen.width - 2)
+		
+//		if (Input.GetMouseButtonDown(1))
+//		{
+//			_rotationStartPosition = Input.mousePosition;
+//		}
+		
+		if (Input.GetMouseButton(1) && !Input.GetKey(CameraControlKeys.RotationLeft) && !Input.GetKey(CameraControlKeys.RotationRight) && !Input.GetMouseButton(0))
+		{					
+			if (Input.mousePosition.x >= Screen.width - 3)
 			{
-				_rotationTarget = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y + (_speedRotationKeyDown * 2 *Time.deltaTime) * _speedNormalizeRotation, transform.eulerAngles.z);
+				_rotationTarget = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y + _rotationSpeedKey /2, transform.eulerAngles.z);
 			}
 			else if (Input.mousePosition.x <= 3)
 			{
-				_rotationTarget = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y + (-_speedRotationKeyDown * 2 * Time.deltaTime) * _speedNormalizeRotation, transform.eulerAngles.z);
+				_rotationTarget = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y -_rotationSpeedKey/2, transform.eulerAngles.z);
 			}
-			else
-			if (Input.GetAxis("Mouse X") >= _sensitivityMousRotationRigth)
+			else if (Input.GetAxis("Mouse X") >= _sensitivityRotationMous)
 			{
-				//_rotationTarget = Quaternion.Euler( transform.eulerAngles.x, transform.eulerAngles.y + (_speedRotationMous * Time.deltaTime) * _speedNormalizeRotation, transform.eulerAngles.z);
-				_rotationTarget = Quaternion.Euler( transform.eulerAngles.x, transform.eulerAngles.y + _speedRotationMous, transform.eulerAngles.z);
+				_rotationTarget = Quaternion.Euler( transform.eulerAngles.x, transform.eulerAngles.y + _rotationSpeed, transform.eulerAngles.z);
 			}
-			else if (Input.GetAxis("Mouse X") <= _sensitivityMousRotationLeft)
+			else if (Input.GetAxis("Mouse X") <= -_sensitivityRotationMous)
 			{
-				//_rotationTarget = Quaternion.Euler( transform.eulerAngles.x, transform.eulerAngles.y + (-_speedRotationMous * Time.deltaTime) * _speedNormalizeRotation, transform.eulerAngles.z);
-				_rotationTarget = Quaternion.Euler( transform.eulerAngles.x, transform.eulerAngles.y - _speedRotationMous, transform.eulerAngles.z);
+				_rotationTarget = Quaternion.Euler( transform.eulerAngles.x, transform.eulerAngles.y - _rotationSpeed, transform.eulerAngles.z);
 			}
+//			else
+//			{
+//				_rotationCurrentPosition = Input.mousePosition;
+//				
+//				Vector3 different = _rotationStartPosition - _rotationCurrentPosition;
+//				
+//				_rotationStartPosition = _rotationCurrentPosition;
+//				
+//				_rotationTarget = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y +(-different.x / 5f) *_rotationSpeed,transform.eulerAngles.z);
+//			}
+			
+			
+			RotationSlerp();
+		}
+	}		
+	
+	private void ZoomLepr()
+	{
+		transform.localPosition = Vector3.Lerp(transform.localPosition,_zoomTarget, Time.deltaTime * _speedLerp);
+	}
+	
+	private void Zoom()
+	{
+		if (Input.GetKey(KeyCode.R))
+		{
+			_zoomTarget.z += _zoomSpeed;
 		}
 		
+		if (Input.GetKey(KeyCode.F))
+		{
+			_zoomTarget.z -= _zoomSpeed;
+		}
+		
+//		if (Input.mouseScrollDelta.y != 0) 
+//		{
+//			_zoomTarget.z +=  _zoomSpeed * 10 * Input.mouseScrollDelta.y;
+//		}
+		_zoomTarget.z = Mathf.Clamp(_zoomTarget.z,_minZoom,_maxZoom);
+	}
+	
+	private void RotationSlerp()
+	{
 		transform.rotation = Quaternion.Slerp(transform.rotation, _rotationTarget, _speedLerp * Time.deltaTime);
-	}		
+	}
+	
+	private void Start()
+	{
+		_zoomTarget = transform.localPosition;
+	}
 }
